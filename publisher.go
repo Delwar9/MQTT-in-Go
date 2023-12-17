6a1f355bcc4461e45206633be22f1458e3ce5e9d
Delwar9 <delwar.hosen@northsouth.edu>
2023-12-17 16:16:45 +0600

    few changes added

--- publisher.go
+++ publisher.go
@@ -10,25 +10,25 @@ import (
 )
 
 func main() {
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(consts.Broker)
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(consts.Broker)
 
-    client := mqtt.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-        panic(fmt.Sprintf("Error connecting to MQTT broker: %s", token.Error()))
-    }
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(fmt.Sprintf("Error connecting to MQTT broker: %s", token.Error()))
+	}
 
-    for i := 1; i <= 20; i++ {
-        message := fmt.Sprintf("Publishing message %d", i)
-        token := client.Publish(consts.Topic, 0, false, message)
-        if token.Wait() && token.Error() != nil {
-            fmt.Printf("Error publishing message %d: %s\n", i, token.Error())
-        } else {
-            fmt.Println("Published:", message)
-        }
+	for i := 1; i <= 20; i++ {
+		message := fmt.Sprintf("Publishing message %d", i)
+		token := client.Publish(consts.Topic, 0, false, message)
+		if token.Wait() && token.Error() != nil {
+			fmt.Printf("Error publishing message %d: %s\n", i, token.Error())
+		} else {
+			fmt.Println("Published:", message)
+		}
 
-        time.Sleep(1 * time.Second)
-    }
+		time.Sleep(1 * time.Second)
+	}
 
-    client.Disconnect(250)
+	client.Disconnect(250)
 }
